91ff1a5143afd4af56c2252551dd2ed8e9fa93dd
Desiree Wilson <desiree.s.wilson@gmail.com>
2022-01-19 11:50:05 -0600

    I created two directories: model and controller. These two directories make up the MVC design pattern. In the model directory, I created a class called User.java. This class models a user (hence why it's in the model directory). I also created a UserController.java file in the controller directory. I think this class will actually retrieve the user data from the database but I cannot remember how. I will have to ask Ruslan.

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User() {}
+    public User(String firstName, String lastName, int age) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+}
