15135c4ee17acf5b664d7ee36602eb47685d9d98
Desiree Wilson <desiree.s.wilson@gmail.com>
2022-02-16 14:39:58 -0600

    I changed corrected the directory 'respository' to 'repository.'

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="api/v1/user")
+public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+    @PostMapping//create
+    String createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping//retrieve
+    User getRequest() {
+        return new User("Desiree", "Wilson", 34);
+    }
+
+    @PutMapping//update
+    void updateUser(Long id, @RequestBody User updateUser) {
+        User existingUser = userService.findById(id);
+        existingUser.setFirstName(updateUser.getFirstName());
+        existingUser.setLastName(updateUser.getLastName());
+        existingUser.setAge(updateUser.getAge());
+        userService.save(existingUser);
+    }
+
+    @DeleteMapping//delete
+    String deleteUser(@RequestBody Long id) {
+        User user = userService.findById(id);
+        return userService.deleteUser(user);
+    }
+}
